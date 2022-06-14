59f01029fd40da72e235c77329d9cbb1ede5fe55
shivanshu32 <shivanshu32@gmail.com>
2022-06-14 19:32:50 +0530

    test

--- src/components/VisNetwork.js
+++ src/components/VisNetwork.js
@@ -4,36 +4,38 @@ import { Network } from 'vis-network';
 const VisNetwork = () => {
 	const container = useRef(null);
 
+  const nodes = [
+    { id: 1, label: 'Protein Sweetener', shape: 'diamond', color: '#F03967' },
+    { id: 2, label: 'Sweetness Index', shape: 'box', color: '#BACDE1'  },
+    { id: 3, label: 'Type of Drink', shape: 'box', color: '#BACDE1'  },
+    { id: 4, label: 'Composition Details',shape: 'box', color: '#BACDE1'  },
+    { id: 5, label: 'Storage Conditions', shape: 'box', color: '#BACDE1'  },
+    { id: 6, label: 'Production Process', shape: 'box', color: '#BACDE1'  },
+    { id: 7, label: 'Benefits', shape: 'box', color: '#BACDE1'  }
+  ];
+
+  const edges = [
+    { from: 1, to: 2 },
+    { from: 1, to: 3 },
+    { from: 1, to: 4 },
+    { from: 1, to: 5 },
+    { from: 1, to: 6 },
+    { from: 1, to: 7 }
+  ];
+
+  const options = {
+    // position: {x:x, y:x},    // position to animate to (Numbers)
+    scale: 2.0,              // scale to animate to  (Number)
+    // offset: {x:x, y:y},      // offset from the center in DOM pixels (Numbers)
+    animation: {             // animation object, can also be Boolean
+      duration: 1000,                 // animation duration in milliseconds (Number)
+      easingFunction: "easeInOutQuad" // Animation easing function, available are:
+    }                                   // linear, easeInQuad, easeOutQuad, easeInOutQuad,
+}    
+
                       
   useEffect(() => {
-    const nodes = [
-      { id: 1, label: 'Protein Sweetener', shape: 'diamond', color: '#F03967' },
-      { id: 2, label: 'Sweetness Index', shape: 'box', color: '#BACDE1'  },
-      { id: 3, label: 'Type of Drink', shape: 'box', color: '#BACDE1'  },
-      { id: 4, label: 'Composition Details',shape: 'box', color: '#BACDE1'  },
-      { id: 5, label: 'Storage Conditions', shape: 'box', color: '#BACDE1'  },
-      { id: 6, label: 'Production Process', shape: 'box', color: '#BACDE1'  },
-      { id: 7, label: 'Benefits', shape: 'box', color: '#BACDE1'  }
-    ];
-  
-    const edges = [
-      { from: 1, to: 2 },
-      { from: 1, to: 3 },
-      { from: 1, to: 4 },
-      { from: 1, to: 5 },
-      { from: 1, to: 6 },
-      { from: 1, to: 7 }
-    ];
-  
-    const options = {
-      // position: {x:x, y:x},    // position to animate to (Numbers)
-      scale: 2.0,              // scale to animate to  (Number)
-      // offset: {x:x, y:y},      // offset from the center in DOM pixels (Numbers)
-      animation: {             // animation object, can also be Boolean
-        duration: 1000,                 // animation duration in milliseconds (Number)
-        easingFunction: "easeInOutQuad" // Animation easing function, available are:
-      }                                   // linear, easeInQuad, easeOutQuad, easeInOutQuad,
-  }    
+    
     const network =
       container.current &&
       new Network(container.current, { nodes, edges }, options);
